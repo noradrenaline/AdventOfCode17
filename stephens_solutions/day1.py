7c33e41c95aa4ba2988da42755766a7a70f7dd6a
Stephen Ingram <singram@riscnetworks.com>
2017-12-01 09:07:09 -0500

    formatting

--- stephens_solutions/day1.py
+++ stephens_solutions/day1.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
+
+"""
+The captcha requires you to review a sequence of digits (your puzzle input) and
+find the sum of all digits that match the next digit in the list. The list is
+circular, so the digit after the last digit is the first digit in the list.
+
+For example:
+
+1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second
+digit and the third digit (2) matches the fourth digit.
+1111 produces 4 because each digit (all 1) matches the next.
+1234 produces 0 because no digit matches the next.
+91212129 produces 9 because the only digit that matches the next one is the
+last digit, 9.
+"""
+
 import sys
 from collections import deque
 A = sys.argv[1]
-B = deque(sys.argv[1])
-B.rotate()
-print sum([int(i[0]) for i in zip(A,B) if i[0] == i[1]])
+B = deque(A)
+B.rotate(n=1)
+print sum([int(i[0]) for i in zip(A, B) if i[0] == i[1]])
